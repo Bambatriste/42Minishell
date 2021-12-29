2449dd2cc48e039813495a3c44a54ee6790d491e
Antoine EDOUARD <aedouard@e2r5p20.clusters.42paris.fr>
2021-12-29 16:30:49 +0100

    first commit branch

---
+++ print.c
@@ -0,0 +1,17 @@
+#include "includes/minishell.h"
+
+
+int main()
+{
+
+    int fd[2];
+
+    //pipe(fd);
+printf("coucou\n");
+//close (fd[0]);
+//close(fd[1]);
+
+//open("toto.c", O_RDONLY);
+
+while(1);
+}
